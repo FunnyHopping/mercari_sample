c18d0127e634f386958fd506479128109d8e9b46
FunnyHopping <dsajikgtre00@yahoo.co.jp>
2019-09-10 14:14:50 +0900

    ENVの整理

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,14 +56,12 @@ set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-set :default_env, { 
-  AWS_ACCESS_KEY_ID: ENV['AWS_ACCESS_KEY_ID'],
-  AWS_SECRET_ACCESS_KEY: ENV['AWS_SECRET_ACCESS_KEY'],
-  DATABASE_USER_NAME: ENV['DATABASE_USER_NAME'],
-  DATABASE_USER_PASS: ENV['DATABASE_PASSWORD'],
-  BASIC_AUTH_USER: ENV['BASIC_AUTH_USER'],
-  BASIC_AUTH_PASSWORD: ENV['BASIC_AUTH_PASSWORD']
-  }
+# set :default_env, { 
+#   AWS_ACCESS_KEY_ID: ENV['AWS_ACCESS_KEY_ID'],
+#   AWS_SECRET_ACCESS_KEY: ENV['AWS_SECRET_ACCESS_KEY'],
+#   DATABASE_USER_NAME: ENV['DATABASE_USER_NAME'],
+#   DATABASE_USER_PASS: ENV['DATABASE_PASSWORD'],
+#   }
 
   set :linked_files, %w{ config/master.key }
 
