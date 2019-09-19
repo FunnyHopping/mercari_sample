6b281df4878b7bea20c769faad46187e5527866d
noguchi16821 <hisu31mari@gmail.com>
2019-09-19 19:00:28 +0900

    itemslickupdate

--- app/assets/javascripts/item-slick.js
+++ app/assets/javascripts/item-slick.js
@@ -1,43 +1,44 @@
 $(document).on('turbolinks:load', function(){
 
   $(function(){
-    var slider = "#slider"; // スライダー
-    var thumbnailItem = "#thumbnail-list .thumbnail-item"; // サムネイル画像アイテム
+    var slider = "#slider"; 
+    var thumbnailItem = "#thumbnail-list .thumbnail-item"; 
     
     // サムネイル画像アイテムに data-index でindex番号を付与
-    $(thumbnailItem).each(function(){
+    $(".thumbnail-item").each(function(){
      var index = $(thumbnailItem).index(this);
      $(this).attr("data-index",index);
     });
     
     // スライダー初期化後、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
-    // 「slickスライダー作成」の前にこの記述は書いてください。
     $(slider).on('init',function(slick){
      var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
-     $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
+     $(".thumbnail-item"+'[data-index="'+index+'"]').addClass("thumbnail-current");
     });
   
     //slickスライダー初期化  
     $(slider).slick({
       arrows: false,
-      infinite: false //これはつけましょう。
+      infinite: false, 
     });
+
+
     //サムネイル画像アイテムをクリックしたときにスライダー切り替え
-    $(thumbnailItem).on('click',function(){
+    $(".thumbnail-item").on('mouseover',function(){
       var index = $(this).attr("data-index");
       $(slider).slick("slickGoTo",index,false);
     });
     
     //サムネイル画像のカレントを切り替え
     $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
-      $(thumbnailItem).each(function(){
+      $(".thumbnail-item").each(function(){
         $(this).removeClass("thumbnail-current");
       });
-      $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
+      $(".thumbnail-item"+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
     });
   });
   
-  $('.thumbnail__item').on( 'mouseover', function() {
+  $(".thumbnail-item").on( 'mouseover', function() {
     $(this).click() //マウスオーバーでクリック
     $(this).css('opacity', 1); //選択した画像のopacityをあげる
     $(this).siblings().css('opacity', 0.5); //それ以外のサムネイル画像のopacityを下げる
