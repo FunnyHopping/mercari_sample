b44cd494abab77ba8e0626b97270ca44cd6fe25e
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-19 10:48:00 +0900

    relationships

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -1,8 +1,9 @@
 class TransactsController < ApplicationController
   def create
-    @item = Item.find(params.require(:transact).require(:item_id))
+    @item = Item.find(transact_params[:item_id])
     @item.transact = true
     @transact = Transact.new(transact_params)
+    @transact.give_id = Item.find(transact_params[:item_id]).saler_id
     if @transact.save!
       if @item.save
         redirect_to root_path
@@ -16,6 +17,6 @@ class TransactsController < ApplicationController
 
   private
   def transact_params
-    params.require(:transact).permit(:evaluat,:confirmat,:body,:item_id)
+    params.require(:transact).permit(:evaluat,:confirmat,:body,:item_id).merge(take_id: current_user.id)
   end
 end
