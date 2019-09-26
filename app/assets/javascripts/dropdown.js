9a100ed1fd9976befa9463b43cc6d3c842d04c6f
noguchi16821 <hisu31mari@gmail.com>
2019-09-26 14:59:07 +0900

    categorynavchange

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,47 +1,44 @@
-  //親カテゴリーメニュー
-  $(function(){
-
-    $('.header__contents__bottom__left__category').hover(function(){
-      $('.parents_list').show();
-    }, function() {
-      $('.parents_list').hide();
-    }); 
+//親カテゴリーメニュー
+$(function(){
+  $('.header__contents__bottom__left__category').hover(function(){
+    $('.parents_list').show();
+  }, function() {
+    $('.parents_list').hide();
   });
+});
 
-    //ブランドメニュー
-  $(function(){
-    $('.header__contents__bottom__left__brand').hover(function(){
-      $('.dropdwn-menu2, .brand-dropdown__list').show();
-    }, function() {
-      $('.dropdwn-menu2, .brand-dropdown__list').hide();
-    });
+//ブランドメニュー
+$(function(){
+  $('.header__contents__bottom__left__brand').hover(function(){
+    $('.dropdwn-menu2, .brand-dropdown__list').show();
+  }, function() {
+    $('.dropdwn-menu2, .brand-dropdown__list').hide();
   });
+});
 
-   // 子カテゴリー
-  $(function() {
-      function buildChildHTML(child){
-        var html =`<a class="child_category" id="${child.id}" 
-                    href="/category/${child.id}">${child.name}</a>`;
-        return html;
-      }
+// 子カテゴリー
+$(function() {
+  function buildChildHTML(child){
+    var html =`<a class="child_category" id="${child.id}" 
+                href="/category/${child.id}">${child.name}</a>`;
+    return html;
+  }
     
-      $(document).on("mouseover", ".parent_category", function () {
-        
-        var id = this.id//どのリンクにマウスが乗ってるのか取得します
-
-        $.ajax({
-          type: 'GET',
-          url: '/pages/new',//とりあえずここでは、newアクションに飛ばしてます
-          data: {parent_id: id},//どの親の要素かを送りますparams[:parent_id]で送られる
-          dataType: 'json'
-        }).done(function(children) {
-          children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
-            var html = buildChildHTML(child);//HTMLにして
-            $(".children_list").append(html);//リストに追加
-          })
-          $(document).on("mouseover", ".parent_category", function () {
-            $(".child_category").remove();
-        });        
+  $(document).on("mouseover", ".parent_category", function () {
+    var id = this.id//どのリンクにマウスが乗ってるのか取得します
+    $.ajax({
+      type: 'GET',
+      url: '/pages/new',//とりあえずここでは、newアクションに飛ばしてます
+      data: {parent_id: id},//どの親の要素かを送りますparams[:parent_id]で送られる
+      dataType: 'json'
+    }).done(function(children) {
+      children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+        var html = buildChildHTML(child);//HTMLにして
+        $(".children_list").append(html);//リストに追加
+      })
+        $(document).on("mouseover", ".parent_category", function () {
+          $(".child_category").remove();
+        });
       });
     });
       // 孫カテゴリ
@@ -51,22 +48,21 @@
         return html;
       }
     
-      $(document).on("mouseover", ".child_category", function () {//子カテゴリーのリストは動的に追加されたHTMLのため
-        var id = this.id
-
-        $.ajax({
-          type: 'GET',
-          url:'/pages/new',
-          data: {parent_id: id},
-          dataType: 'json'
-        }).done(function(children) {
-          children.forEach(function (child) {
-            var html = buildGrandChildHTML(child);
-            $(".grand_children_list").append(html);
-          })
-          $(document).on("mouseover", ".child_category, .parent_category", function () {
+    $(document).on("mouseover", ".child_category", function () {//子カテゴリーのリストは動的に追加されたHTMLのため
+      var id = this.id
+      $.ajax({
+        type: 'GET',
+        url:'/pages/new',
+        data: {parent_id: id},
+        dataType: 'json'
+      }).done(function(children) {
+        children.forEach(function (child) {
+          var html = buildGrandChildHTML(child);
+          $(".grand_children_list").append(html);
+        })
+          $(document).on("mouseover", ".child_category", function () {
             $(".grand_child_category").remove();
-          });
         });
       });
-    });  
+    });
+  }); 
